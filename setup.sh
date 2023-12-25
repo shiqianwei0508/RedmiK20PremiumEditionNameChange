70a909e4c39c08903ff5d31983833839c543aa85
sqwei <m18610844053@163.com>
2023-12-25 16:58:41 +0800

    update

--- setup.sh
+++ setup.sh
@@ -31,6 +31,11 @@ REPLACE_EXAMPLE="
 "
 # Construct your own list here.
 REPLACE="
+/system/product/etc/build.prop
+/system/system_ext/etc/build.prop
+/system/build.prop
+/vendor/odm/etc/build.prop
+/vendor/build.prop
 "
 
 ############
@@ -50,7 +55,7 @@ set_permissions() {
 # Set what you want to be displayed on header of installation process.
 info_print() {
   ui_print ""
-  print_title "MMT Reborn" "By @iamlooper @ telegram"
+  print_title "change build prop"
 }
 
 ############
@@ -59,6 +64,100 @@ info_print() {
 
 # Change the logic to whatever you want.
 init_main() {
-  ui_print ""
-  ui_print "Hello World!"
+#  ui_print ""
+#  ui_print "Hello World!"
+ui_print "[*] Initialising Setup...."
+
+ui_print "[*] BackingUp Prop File to Internal Storage"
+
+# Backup Code
+# 源文件列表
+source_files=(
+    "/system/product/etc/build.prop"
+    "/system/system_ext/etc/build.prop"
+    "/system/build.prop"
+    "/vendor/odm/etc/build.prop"
+    "/vendor/build.prop"
+)
+
+# 目标目录
+prop_edit_directory="/sdcard/buildpropBackup"
+
+# 遍历源文件列表，并将每个文件复制到目标目录中
+for source_file in "${source_files[@]}"
+do
+    if [ -f "$source_file" ]; then
+        target_file="$prop_edit_directory${source_file#/*}"
+        target_directory=$(dirname "$target_file")
+
+        # 检查目标目录是否存在，如果不存在则创建目录
+        if [ ! -d "$target_directory" ]; then
+            mkdir -p "$target_directory"
+            echo "Directory $target_directory created."
+        fi
+
+        cp "$source_file" "$target_file"
+        echo "Copied $source_file to $target_file"
+    else
+        echo "Source file $source_file does not exist."
+    fi
+done
+
+# Main Part of the script
+
+ui_print "[*] Making Systemless Build.prop In Module"
+
+#cp /system/build.prop /$MODPATH/system/
+
+# 源文件列表
+#source_files=(
+#    "/system/product/etc/build.prop"
+#    "/system/system_ext/etc/build.prop"
+#    "/system/build.prop"
+#    "/vendor/odm/etc/build.prop"
+#    "/vendor/build.prop"
+#)
+
+# 目标目录
+prop_edit_directory_formodule="/$MODPATH"
+
+# 遍历源文件列表，并将每个文件复制到目标目录中
+for source_file in "${source_files[@]}"
+do
+    if [ -f "$source_file" ]; then
+        target_file="$prop_edit_directory_formodule${source_file#/*}"
+        target_directory=$(dirname "$target_file")
+
+        # 检查目标目录是否存在，如果不存在则创建目录
+        if [ ! -d "$target_directory" ]; then
+            mkdir -p "$target_directory"
+            echo "Directory $target_directory created."
+        fi
+
+        cp "$source_file" "$target_file"
+        echo "Copied $source_file to $target_file"
+    else
+        echo "Source file $source_file does not exist."
+    fi
+done
+
+
+ui_print "[*] Done..."
+
+
+ui_print "[*] Checking Installation..."
+if [ -f "$MODPATH/system/build.prop" ]; then
+    echo "Sucessfully Installed"
+else
+    echo "Something went wrong , Exiting..."
+    exit 1
+fi
+
+
+
+ui_print "[*] Reboot to Use Systemless Build.prop"
+
+ui_print "[*] All Edits to build.prop will be systemlessly perform , in case of issue just disable or Uninstall this Module"
+
+ui_print "All Done.."
 }
